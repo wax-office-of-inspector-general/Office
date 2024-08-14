4544e445c47ffe235398c1c7ab8b71ba5b470214
rakeden <5896201+rakeden@users.noreply.github.com>
2024-08-14 18:51:47 +0200

    finalize notion-tools to generate scores payload

--- notion-tools/index.js
+++ notion-tools/index.js
@@ -1,30 +1,34 @@
-require('dotenv').config();
-const { Client } = require('@notionhq/client');
+require("dotenv").config();
+const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 (async () => {
-  const databaseId = 'aec8e648244c4b42a19d172f9fe9f1aa';
+  const databaseId = "aec8e648244c4b42a19d172f9fe9f1aa";
   const response = await notion.databases.query({
     database_id: databaseId,
     sorts: [
       {
-        property: 'BP account',
-        direction: 'ascending',
+        property: "BP account",
+        direction: "ascending",
       },
     ],
   });
 
-  let scores = []; 
+  let scores = [];
 
-  response.results.map(res => {
+  response.results.map((res) => {
     scores.push({
-      "guild": res.properties['BP account'].rich_text[0].text.content,
-      "score": res.properties.Total.number * 10000
+      guild: res.properties["BP account"].rich_text[0].text.content,
+      score: res.properties.Total.number * 10000,
     });
   });
 
-  console.log(`${scores.length} guilds were scored`);
-  console.log(scores);
+  console.log(`${scores.length} Guilds scored in total\n\n`);
+  
+  console.log('Review the scores below and make sure they are correct before proceeding\n\n');
 
+  console.log(
+    JSON.stringify(scores)
+  );
 })();
